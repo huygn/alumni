05d10f39202372ed12b114607020d0c156a46047
Huy Giang <gnhuy91@gmail.com>
2018-11-12 22:11:25 +0700

    update

--- src/services/footer.js
+++ src/services/footer.js
@@ -1,28 +1,3 @@
-// import axios from 'axios'
-// import cheerio from 'cheerio'
-// import fs from 'fs'
-
-// export const crawlFooter = async () => {
-//   const { data } = await axios.get('https://dwarves.foundation ')
-//   const $ = cheerio.load(data)
-//   const html = $('footer').html()
-//   // write to file and later feed it to PurgeCSS
-//   await writeFile('./footer.js', `module.exports = '${html}'`, { flag: 'w+' })
-//   return html
-// }
-
-// function writeFile(path, data, opt) {
-//   return new Promise((resolve, reject) => {
-//     fs.writeFile(path, data, opt, err => {
-//       if (err) {
-//         reject(err)
-//         return
-//       }
-//       resolve()
-//     })
-//   })
-// }
-
 import preval from 'preval.macro'
 
 export const footerHTML = preval`
