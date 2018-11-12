47d614618a0ce5adb09acbaf02fdc0af874e3fef
Huy Giang <gnhuy91@gmail.com>
2018-11-12 15:41:34 +0700

    update

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { withSiteData } from 'react-static'
+import preval from 'preval.macro'
 
-const PageFooter = withSiteData(({ footerHTML, ...rest }) => {
+const footerHTML = preval`
+const request = require('sync-request')
+const cheerio = require('cheerio')
+
+const res = request('GET', 'https://dwarves.foundation')
+const $ = cheerio.load(res.getBody().toString())
+const footer = $('footer').html()
+
+module.exports = footer
+`
+
+const PageFooter = ({ ...rest }) => {
   return <footer {...rest} dangerouslySetInnerHTML={{ __html: footerHTML }} />
-})
+}
 
 export default PageFooter
